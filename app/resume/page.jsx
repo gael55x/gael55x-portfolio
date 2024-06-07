00b36a09ce5149c91556b4b0c34914075ba9a8b6
gael55x <gailleamolong5@gmail.com>
2024-06-07 21:02:35 +0800

    Added imports

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,18 @@
+"use client";
+
+import {
+    FaReact, 
+    FaFigma, 
+    FaNodeJs,
+    FaFlask, 
+    FaNextjs, 
+    FaTailwindCss,
+    FaJuypter,
+    FaHTML5, 
+    FaCss3, 
+    FaJs, 
+} from 'react-icons/fa';
+
 const Resume = () => {
     return <div>resume page</div>
 };
