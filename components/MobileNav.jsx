0d2e5917510d1636dc4d7cc79afe20aa0e128e94
gael55x <gailleamolong5@gmail.com>
2024-06-07 15:55:09 +0800

    finalized navbar for mobile responsiveness

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -40,7 +40,21 @@ export default function MobileNav() {
         </SheetTrigger>
         <SheetContent className="flex flex-col">
             {/* logo */}
-            
+            <div className="mt-32 mb-40 text-center text-2xl">
+                <Link href='/' className='text-4xl font-semibold'>
+                    <h1>Gaille<span className='text-accent'>.</span></h1>
+                </Link>
+            </div>
+            <nav className="flex flex-col justify-center items-center gap-8">
+                {links.map((link, index)=> {
+                    return (
+                        <Link href={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2 border-accent"} 
+                        text-xl capitalize hover:text-accent transition-all`}>
+                            {link.name}
+                        </Link>
+                    );
+                })}
+            </nav>;
         </SheetContent>
     </Sheet>
     );
