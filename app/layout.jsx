a39d2737695bf5cc908145f18c9a63d617a08688
gael55x <gailleamolong5@gmail.com>
2024-06-07 14:29:47 +0800

    navbar initializations

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,9 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
+// components
+import Header from "@/components/Header";
+
 const jetbrainsMono = JetBrains_Mono(
   { 
     subsets: ["latin"], 
@@ -17,7 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jetbrainsMono.variable}>{children}</body>
+      <body className={jetbrainsMono.variable}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
