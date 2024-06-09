1609c531c4a616d160f8a417e0502be7a3e7642c
gael55x <gailleamolong5@gmail.com>
2024-06-09 16:19:56 +0800

    Added workslider buttons

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -11,6 +11,7 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/comp
 
 import Link from "next/link";
 import Image from "next/image";
+import WorkSliderBtns from "@/components/WorkSliderBtns";
 
 const projects = [
     {
@@ -54,9 +55,16 @@ const projects = [
 const Work = () => {
     const [project, setProject] = useState(projects[0]);
 
+    const handleSlideChange = (swiper) => {
+        // get current slide index
+        const currentIndex = swiper.activeIndex;
+        // update project state based on current slide index
+        setProject(projects[currentIndex])
+    }
+
     return (
         <motion.div initial={{opacity: 0}} 
-        animate={{opacity: 1}} className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
+        animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: 'easeIn'}}}className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0">
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row xl:gap-[30px]">
                     <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
@@ -115,7 +123,24 @@ const Work = () => {
                              </div>
                         </div>
                     </div>
-                    <div className="w-full xl:w-[50%]">slider</div>
+                    <div className="w-full xl:w-[50%]">
+                        <Swiper spaceBetween={30} slidesPerView={1} className="xl:h-[520px] mb-12" onSlideChange={handleSlideChange}>
+                            {projects.map((project, index) => {
+                                return <SwiperSlide key={index} className="w-full">
+                                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                                        {/* overlay */}
+                                        <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10 "></div>
+                                        {/* image */}
+                                        <div className="relative w-full h-full">
+                                            <Image src={project.image} fill className="object-cover" alt />
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                            })}
+                            {/* buttons */}
+                            <WorkSliderBtns />
+                        </Swiper>
+                    </div>
                 </div>
             </div>
         </motion.div>
