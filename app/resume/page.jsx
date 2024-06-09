deed1d70f7fa36d7fb9312941ba50d14372fbb62
gael55x <gailleamolong5@gmail.com>
2024-06-09 12:46:22 +0800

    Added experience section

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -66,7 +66,7 @@ const experience = {
         {
             company: "Bitwork solutions",
             position: "Software Engineer", 
-            duration: "August 2023 - present"
+            duration: "September 2023 - present"
         },
         {
             company: "TidalStack",
@@ -75,7 +75,7 @@ const experience = {
         },
         {
             company: "Minglanilla Science High School",
-            position: "Head Developer", 
+            position: "Head Developer - Volunteer Work", 
             duration: "March 2023 - March 2024"
         },
     ]
@@ -264,6 +264,9 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { motion } from 'framer-motion';
+import { Link } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { FiDownload } from 'react-icons/fi';
 
 const Resume = () => {
     return (
@@ -288,8 +291,28 @@ const Resume = () => {
                     <div className="min-h-[70vh] w-full">
                         {/* experience */}
                         <TabsContent value="experience" className="w-full">
-                            
-                            experience
+                            <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                                <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>
+                                    {experience.description}
+                                </p>
+                                <ScrollArea className="h-[400px]">
+                                    <ul className='grid grid-cols-1 lg:grid-cols-2 gap-[30px]'>
+                                        {experience.items.map((item, index) => {
+                                            return <li key={index} className='bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1'>
+                                                <span className='text-accent'>{item.duration}</span>
+                                                <h3 className='text-xl max-w-[260px] min-h-[60px] 
+                                                text-center lg:text-left'>{item.position}</h3>
+                                                <div className='flex items-center gap-3'>
+                                                    {/* dot */}
+                                                    <span className='w-[6px] h-[6px] rounded-full bg-accent'></span>
+                                                    <p className='text-white/60'>{item.company}</p>
+                                                </div>
+                                            </li>
+                                        })}
+                                    </ul>
+                                </ScrollArea>
+                            </div>
                         </TabsContent>
                         {/* education */}
                         <TabsContent value="education" className="w-full">
